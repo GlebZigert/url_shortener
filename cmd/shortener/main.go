88b3abc1e8410d57f170128e8c5b43e35be061b1
GlebZigert <gleb.zigert@yandex.ru>
2024-05-31 18:52:25 +0300

    add server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+
+	/*add server to fix
+	Не удалось дождаться пока порт 8080 станет доступен для запроса: context deadline exceeded
+	*/
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
+}
