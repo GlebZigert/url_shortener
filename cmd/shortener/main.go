25362f64ffc564570e1a83aad6912ba2444ae16d
GlebZigert <gleb.zigert@yandex.ru>
2024-06-09 19:37:08 +0300

    .

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -97,8 +97,8 @@ var BaseURL string
 func ParseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
-	flag.StringVar(&RunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&BaseURL, "b", "http://localhost", "base address for short URL")
+	flag.StringVar(&RunAddr, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&BaseURL, "b", "http://localhost:8080", "base address for short URL")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
 }
