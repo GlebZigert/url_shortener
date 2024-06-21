945d20616bbf3a5abbbdb88337d3e8b3d0d21bef
GlebZigert <GlebZigert>
2024-06-21 11:11:17 +0300

    .

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/GlebZigert/url_shortener.git/internal/app"
+
+	"go.uber.org/zap"
 )
 
+var sugar zap.SugaredLogger
+
 func main() {
+
 	app.Run()
 }
--- internal/transport/router.go
+++ internal/transport/router.go
@@ -5,18 +5,20 @@ import (
 	"net/http"
 
 	"github.com/GlebZigert/url_shortener.git/internal/config"
+	"github.com/GlebZigert/url_shortener.git/internal/logger"
 	"github.com/go-chi/chi"
 )
 
 //Назначение роутера
 
 func InitRouter() {
+
 	r := chi.NewRouter()
 
 	// r.Get(`/`, Get)
 
-	r.Post(`/`, CreateShortURL)
-	r.Get(`/*`, GetURL)
+	r.Post(`/`, logger.RequestLogger(CreateShortURL))
+	r.Get(`/*`, logger.RequestLogger(GetURL))
 
 	log.Fatal(http.ListenAndServe(config.RunAddr, r))
 }
