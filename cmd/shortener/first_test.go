18bda1fb4f769cb376d137647605b02de3516915
GlebZigert <GlebZigert>
2024-06-25 08:02:02 +0300

    .

--- cmd/shortener/first_test.go
+++ cmd/shortener/first_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/GlebZigert/url_shortener.git/internal/server"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
