8d7a4cd137c5b27c95ff4f19bc77b799a5aa53aa
di <nah>
2024-08-28 10:32:42 +0300

    .

---
+++ internal/auth/auth.go
@@ -0,0 +1,67 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var userid int
+
+// Claims — структура утверждений, которая включает стандартные утверждения
+// и одно пользовательское — UserID
+type Claims struct {
+	jwt.RegisteredClaims
+	UserID int
+}
+
+const TOKEN_EXP = time.Hour * 3
+const SECRET_KEY = "supersecretkey"
+
+// BuildJWTString создаёт токен и возвращает его в виде строки.
+func BuildJWTString() (string, error) {
+	// создаём новый токен с алгоритмом подписи HS256 и утверждениями — Claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			// когда создан токен
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(TOKEN_EXP)),
+		},
+		// собственное утверждение
+		UserID: userid,
+	})
+
+	userid = userid + 1
+
+	// создаём строку токена
+	tokenString, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+
+	// возвращаем строку токена
+	return tokenString, nil
+}
+
+func GetUserId(tokenString string) (int, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(SECRET_KEY), nil
+		})
+	if err != nil {
+		return -1, err
+	}
+
+	if !token.Valid {
+		fmt.Println("Token is not valid")
+		return -1, errors.New("Token is not valid")
+	}
+
+	fmt.Println("Token os valid")
+	return claims.UserID, nil
+}
---
+++ internal/middleware/withAuth.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/GlebZigert/url_shortener.git/internal/auth"
+)
+
+func AuthMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// по умолчанию устанавливаем оригинальный http.ResponseWriter как тот,
+		// который будем передавать следующей функции
+		ow := w
+
+		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+		authv := r.Header.Get("Authorization")
+		fmt.Println("auth: ", authv)
+
+		userid, err := auth.GetUserId(authv)
+		ctx := r.Context()
+		if err != nil {
+			fmt.Println("auth err: ", err)
+			jwt, _ := auth.BuildJWTString()
+			userid, _ = auth.GetUserId(jwt)
+			ctx = context.WithValue(ctx, "jwt", jwt)
+		}
+
+		ctx = context.WithValue(ctx, "user", userid)
+		r = r.WithContext(ctx)
+		h.ServeHTTP(ow, r)
+	}
+}
