8434f922ecc34e4bdc181d9982298ca597c02da7
GlebZigert <GlebZigert>
2024-09-06 13:05:44 +0300

    паттерн guard в Batcher

--- internal/server/Batcher.go
+++ internal/server/Batcher.go
@@ -22,47 +22,49 @@ type BatchBack struct {
 
 func Batcher(w http.ResponseWriter, req *http.Request) error {
 
-	if req.Method == http.MethodPost {
-
-		var batches []Batch
-
-		var buf bytes.Buffer
-
-		_, err := buf.ReadFrom(req.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return err
-		}
-
-		if err := json.Unmarshal(buf.Bytes(), &batches); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return err
-		}
-		ll := len(batches)
-		batchback := make([]BatchBack, ll)
-		var conflict *services.ErrConflict409
-		for i, b := range batches {
-
-			ress, err := services.Short(b.OriginalURL, -1)
-			if err == nil || errors.As(err, &conflict) {
-				res := config.BaseURL + "/" + ress
-				batchback[i] = BatchBack{b.CorrelationID, res}
-			}
-		}
-
-		resp, err := json.Marshal(batchback)
-		if err != nil {
-
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return err
-		}
-
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-
-		w.Write(resp)
-
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return errors.New("req.Method != http.MethodPost")
 	}
+
+	var batches []Batch
+
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return err
+	}
+
+	if err := json.Unmarshal(buf.Bytes(), &batches); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return err
+	}
+	ll := len(batches)
+	batchback := make([]BatchBack, ll)
+	var conflict *services.ErrConflict409
+	for i, b := range batches {
+
+		ress, err := services.Short(b.OriginalURL, -1)
+		if err == nil || errors.As(err, &conflict) {
+			res := config.BaseURL + "/" + ress
+			batchback[i] = BatchBack{b.CorrelationID, res}
+		}
+	}
+
+	resp, err := json.Marshal(batchback)
+	if err != nil {
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	w.Write(resp)
+
 	return nil
 
 }
