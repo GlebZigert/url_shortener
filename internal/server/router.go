192bd52f2326c3618e82b1263209982288124238
GlebZigert <GlebZigert>
2024-09-09 11:52:45 +0300

    rotate Auth and Log

--- internal/server/router.go
+++ internal/server/router.go
@@ -13,13 +13,13 @@ func InitRouter() {
 	r := chi.NewRouter()
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.ErrHandler)
-		r.Post(`/`, middleware.Auth(middleware.Log(middleware.Gzip(CreateShortURL))))
+		r.Post(`/`, middleware.Log(middleware.Auth(middleware.Gzip(CreateShortURL))))
 		r.Post(`/api/shorten`, middleware.Log(CreateShortURLfromJSON))
 		r.Post(`/api/shorten/batch`, middleware.Log(Batcher))
-		r.Get(`/api/user/urls`, middleware.Auth(middleware.Log(GetURLs)))
+		r.Get(`/api/user/urls`, middleware.Log(middleware.Auth(GetURLs)))
 		r.Get(`/ping`, middleware.Log(Ping))
 		r.Get(`/*`, middleware.Log(GetURL))
-		r.Delete(`/api/user/urls`, middleware.Auth(middleware.Log(Delete)))
+		r.Delete(`/api/user/urls`, middleware.Log(middleware.Auth(Delete)))
 
 	})
 
