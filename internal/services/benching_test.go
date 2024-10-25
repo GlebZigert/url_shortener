3904f42a88bc534a7f81b0cd1ccbe7c765db27ab
GlebZigert <GlebZigert>
2024-10-25 08:27:16 +0300

    add bench test

---
+++ internal/services/benching_test.go
@@ -0,0 +1,29 @@
+package services
+
+import (
+	"context"
+	"strconv"
+	"testing"
+
+	"github.com/GlebZigert/url_shortener.git/internal/config"
+	"github.com/GlebZigert/url_shortener.git/internal/db"
+	"github.com/GlebZigert/url_shortener.git/internal/logger"
+	"github.com/GlebZigert/url_shortener.git/internal/storager"
+)
+
+func BenchmarkSimplest(b *testing.B) {
+
+	cfg := config.NewConfig()
+	ctx := context.Background()
+
+	db.Init(cfg.DatabaseDSN)
+	store := storager.New(cfg)
+
+	logger := logger.NewLogrusLogger(cfg.FlagLogLevel, ctx)
+
+	service := NewService(logger, store)
+	for i := 0; i < 100; i++ {
+		service.Short(strconv.Itoa(i), 0)
+	}
+
+}
