27a503b2377ef65b9426872471e3009cd1a27db0
GlebZigert <GlebZigert>
2024-11-21 11:16:16 +0300

    db storager test

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 )
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
---
+++ internal/storager/db_storager_test.go
@@ -0,0 +1,52 @@
+package storager
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/GlebZigert/url_shortener.git/internal/db"
+	"github.com/GlebZigert/url_shortener.git/storemocks"
+	"github.com/golang/mock/gomock"
+)
+
+func TestDBStoragerLoad(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	cfg := storemocks.NewMockStoreConfig(ctrl)
+
+	tdb, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer tdb.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "title", "body"}).
+		AddRow(1, "post 1", "hello").
+		AddRow(2, "post 2", "world")
+
+	mock.ExpectQuery("SELECT * FROM strazh").WillReturnRows(rows)
+
+	store, err := NewDBStorager(cfg, db.Get(tdb))
+
+	tests := []struct {
+		name string
+		len  int
+	}{
+		{
+			name: "1",
+			len:  1,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			var shorten []*Shorten
+			store.Load(&shorten)
+
+			t.Log(len(shorten))
+
+		})
+	}
+
+}
