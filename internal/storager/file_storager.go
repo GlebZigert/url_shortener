7824e835a7b923ba5178b093926d597cc5a1db23
ввв <>
2024-11-29 11:29:31 +0300

    test for file_storager mrthod WriteUID

--- internal/storager/file_storager.go
+++ internal/storager/file_storager.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
+	"strconv"
 )
 
 var id int
@@ -93,5 +94,7 @@ func (one *FileStorager) StorageWrite(short, origin string, UUID int) error {
 
 func (one *FileStorager) WriteUID(uid int) error {
 	//пока заглушка
-	return nil
+	err := one.rw.Write([]byte("User: "+strconv.Itoa(uid)+"\n"), one.cfg.GetFileStoragePath())
+
+	return err
 }
