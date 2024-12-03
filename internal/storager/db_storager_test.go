4b453fa90c64cf4f5abba263058854c0def80a9e
GlebZigert <GlebZigert>
2024-12-03 08:38:28 +0300

    test for success load short table from db

--- internal/storager/db_storager_test.go
+++ internal/storager/db_storager_test.go
@@ -21,6 +21,52 @@ func TestDBStoragerLoad(t *testing.T) {
 	}
 	defer tdb.Close()
 
+	rows := sqlmock.NewRows([]string{"id", "uid", "origin", "short", "deleted"}).
+		AddRow(1, 1, "11111", "22222", false).
+		AddRow(2, 3, "post 2", "world", true)
+
+	mock.ExpectQuery("SELECT * FROM strazh").WillReturnRows(rows)
+
+	store, err := NewDBStorager(cfg, db.Get(tdb))
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	tests := []struct {
+		name string
+		len  int
+	}{
+		{
+			name: "1",
+			len:  1,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			var shorten []*Shorten
+			store.Load(&shorten)
+
+			t.Log(len(shorten))
+
+		})
+	}
+
+}
+
+func TestDBStoragerLoad01(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	cfg := storemocks.NewMockStoreConfig(ctrl)
+
+	tdb, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer tdb.Close()
+
 	rows := sqlmock.NewRows([]string{"id", "title", "body"}).
 		AddRow(1, "post 1", "hello").
 		AddRow(2, "post 2", "world")
