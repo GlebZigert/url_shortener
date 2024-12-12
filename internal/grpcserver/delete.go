ccd221d77815fa69d00410cb0d262801b86f0c4a
GlebZigert <GlebZigert>
2024-12-12 11:26:18 +0300

    delete...

--- internal/grpcserver/delete.go
+++ internal/grpcserver/delete.go
@@ -5,17 +5,34 @@ import (
 	"log"
 
 	pb "github.com/GlebZigert/url_shortener.git/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *UrlShortenerServer) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteResponce, error) {
-	var resp pb.DeleteResponce
-	var err error
 
 	log.Println("grpc Delete endpoint")
+
+	user, ok := s.auc.CheckUID(ctx)
+	if !ok {
+		s.logger.Error("Нет данных о пользователе: ", map[string]interface{}{})
+
+		return nil, status.Error(codes.Internal, "")
+	}
 
 	for _, v := range in.Todel {
 		log.Println(v)
 	}
 
-	return &resp, err
+	go func() {
+		cerr := s.service.Delete(in.Todel, user)
+		if cerr != nil {
+			s.logger.Error("Delete ", map[string]interface{}{
+				"err": cerr.Error(),
+			})
+
+		}
+	}()
+
+	return nil, nil
 }
